9fb5c1e56539a02b7e97530aaca1b1944a498313
d.abazi <d.abazi@pythys.com>
2022-12-27 00:40:13 +0530

    initial commit

---
+++ student_management_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    objects = models.Manager()
