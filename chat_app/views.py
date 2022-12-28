31222d526d984890dd0427fe3c9a4acfcddb4a96
d.abazi <d.abazi@pythys.com>
2022-12-29 02:59:51 +0530

    Add Topic, Room, Message models to chat app

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
-
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'chat_app/home.html', context)
 
 def room(request, roomId):
-    room = None
-    for i in rooms:
-        if i['id'] == int(roomId):
-            room = i
+    room = Room.objects.get(id=roomId)
     context = {'room': room}
     return render(request, 'chat_app/room.html', context)
