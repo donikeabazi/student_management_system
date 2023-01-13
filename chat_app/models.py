8a808661fa477cad790481ed6331d7377acf9168
d.abazi <d.abazi@pythys.com>
2023-01-13 19:59:13 +0530

    configured settings to use the custom user in management app and re applied all migrations to avoid corrupted cache

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
@@ -9,7 +9,7 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
@@ -17,11 +17,14 @@ class Room(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated_at', 'created_at']
+
     def __str__(self) -> str:
         return self.name
 
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
--- chat_app/urls.py
+++ chat_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/<str:roomId>', views.room, name="room"),
+    
+    path('room/add', views.createRoom, name="add-room"),
+    path('room/<str:id>', views.room, name="room"),
+    path('room/<str:id>/update', views.updateRoom, name="update-room"),
+    path('room/<str:id>/delete', views.deleteRoom, name="delete-room"),
 ]
